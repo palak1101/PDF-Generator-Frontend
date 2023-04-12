d228a7c25b766bdcb725c0952ecd4a6045e3c186
Palak Tiwari <palaktiwari@CJ-134.local>
2023-04-12 05:07:55 -0700

    added crud functionality get and delete

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
-// import "./Home.css";
 import axios from "axios";
+import {useParams} from "react-router-dom";
+
+
 
 const Home = () => {
 
-    const [users, setUsers] = useState([]);
+    const [templates, setTemplates] = useState([]);
+
+
+    const loadTemplates = async () => {
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/templates/`);
+        console.log(result);
+        setTemplates(result.data.data);
+    }
+
+    const deleteUser = async (id) => {
+
+        try {
+            const res2 = await axios.delete(`${process.env.REACT_APP_BASE_URL}/templates/${id}`);
+            loadTemplates();
+        } catch (error) {
+            console.log(error) 
+        } 
+        
+    }
+
 
     useEffect(() => {
         // console.log("My App");
-        loadUsers();
-    }, [users])
-
-
-    const loadUsers = async () => {
-        const result = await axios.get("http://localhost:3001/users");
-        // console.log(result);
-        setUsers(result.data);
-    }
+        loadTemplates();
+    }, [])
 
 
 
@@ -26,13 +40,11 @@ const Home = () => {
             <div className="py-4">
                 <h4>Templates</h4>
                 <br></br>
-                <table class="table">
+                <table className="table">
                     <thead>
                         <tr className="bg-dark text-white">
                             <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Email</th>
+                            <th scope="col">Title</th>
                             <th scope="col">Action</th>
                         </tr>
                     </thead>
@@ -42,16 +54,16 @@ const Home = () => {
                             <td>Mark</td>
                             <td>Otto</td>
                             <td>@mdo</td> */}
-                        {users.map((user, index) => (
+                        {templates.map((template, index) => (
                             <tr>
                                 <th scope="row">{index + 1}</th>
-                                <td>{user.name}</td>
-                                <td>{user.username}</td>
-                                <td>{user.email}</td>
+                                <td>{template.title}</td>
+                                {/* <td>{templates.htm}</td>
+                                <td>{user.email}</td> */}
                                 <td>
-                                    <Link className="btn btn-primary m-2"><i class="fa-sharp fa-solid fa-eye"></i></Link>
-                                    <Link className="btn btn-success m-2"><i class="fa-solid fa-pencil"></i></Link>
-                                    <Link className="btn btn-danger m-2"><i class="fa-solid fa-trash"></i></Link>
+                                    <Link className="btn btn-primary m-2"><i className="fa-sharp fa-solid fa-eye"></i></Link>
+                                    <Link className="btn btn-success m-2"><i className="fa-solid fa-pencil"></i></Link>
+                                    <Link className="btn btn-danger m-2" onClick={() => deleteUser(template.template_id)}><i className="fa-solid fa-trash"></i></Link>
                                 </td>
                             </tr>
                         ))}
